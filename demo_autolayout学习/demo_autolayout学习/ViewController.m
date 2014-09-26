62589b514efff856fa3865ccb6e8ea937f3ec718
Lwgfangz <lwgfangz@192.168.200.129>
2014-09-26 18:11:21 +0800

    1

---
+++ demo_autolayout学习/demo_autolayout学习/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  demo_autolayout学习
+//
+//  Created by Lwgfangz on 14-9-24.
+//  Copyright (c) 2014年 Lwgfangz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ demo_autolayout学习/demo_autolayout学习/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  demo_autolayout学习
+//
+//  Created by Lwgfangz on 14-9-24.
+//  Copyright (c) 2014年 Lwgfangz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
